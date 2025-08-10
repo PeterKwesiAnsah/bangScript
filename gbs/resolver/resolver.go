b817453129cd066ff5d6fdc468212c8a3f81d7ce
peterkwesiansah <ansahpeter123@yahoo.com>
2025-08-10 11:38:27 +0000

    chore: skeleton implementation for resolver

--- gbs/resolver/resolver.go
+++ gbs/resolver/resolver.go
@@ -1,1 +1,15 @@
 package resolver
+
+import "bangScript/gbs/parser"
+
+type Resolved interface {
+	Varesolution(env *parser.Stmtsenv) error
+}
+
+type ResVarStmt struct {
+	tree *parser.VarStmt
+}
+
+func (t ResVarStmt) Varesolution(env *parser.Stmtsenv) error
+
+func Resolver(stmts []parser.Stmt) []Resolved
