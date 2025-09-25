912f34a543394d21b76c23ce06859eaafb2535b8
peterkwesiansah <ansahpeter123@yahoo.com>
2025-09-25 20:37:16 +0000

    read file script into buffer

--- cbs/main.c
+++ cbs/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+
+const char *src;
 int main(int argc,char *args[]){
-return 0;
+    if (argc==1){
+    //run in REPL
+    }else if(argc==2){
+        //run in script mode
+        FILE *fp=fopen(args[1], "r");
+        if (fp==NULL){
+            fprintf(stderr, "Failed to open File.Perhaps path to file is incorrect");
+            exit(1);
+        }
+        fseek(fp, 0L, SEEK_END);
+        size_t fileSize = ftell(fp);
+        rewind(fp);
+        char *buffer=(char *)malloc(fileSize + 1);
+        size_t bytesRead = fread(buffer, sizeof(char), fileSize, fp);
+        buffer[bytesRead] = '\0';
+        src=buffer;
+        fclose(fp);
+    }else{
+        fprintf(stderr,"Usage: %s [path to script]\n",args[0]);
+        exit(1);
+    }
+    return 0;
 }
