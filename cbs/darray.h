1d017c369cae9c5ac6a661f56d286054958974ef
peterkwesiansah <ansahpeter123@yahoo.com>
2025-10-06 16:05:13 +0000

    add DECLARE_ARRAY macro

--- cbs/darray.h
+++ cbs/darray.h
@@ -3,18 +3,23 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+
 #define slice(type,name) \
 typedef struct {   \
     size_t cap;\
     size_t len;\
     type *arr;\
 } name;
+
+#define DECLARE_ARRAY(type, name) \
+  struct { size_t cap, len; type *arr; } name
+
 #define DEFAULT_SLICE_CAP 256
 #define append(slice,el,size) do{ \
     if (slice.len >= slice.cap) \
        { \
         size_t allocap=slice.cap == 0 ? DEFAULT_SLICE_CAP: (2 * slice.cap);\
-        \**Todo: Take from arena memory**/\
         void *ptr = realloc(slice.arr,allocap*size); \
         if (ptr!=NULL) { \
             slice.cap=allocap;  \
@@ -23,4 +28,5 @@ typedef struct {   \
         }else{ fputs("Not enough memory",stderr); exit(1); } \
     } else{ slice.arr[slice.len++]=el; } \
 } while(0)
+
 #endif
