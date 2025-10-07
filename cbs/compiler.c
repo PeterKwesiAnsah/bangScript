87a2c7e469792b0af6a4bb15d2aafb8f4fd7594b
peterkwesiansah <ansahpeter123@yahoo.com>
2025-10-07 21:14:48 +0000

    zero constant

--- cbs/compiler.c
+++ cbs/compiler.c
@@ -1,9 +1,11 @@
 #include "compiler.h"
 #include "scanner.h"
 #include "parser.h"
+#include "readonly.h"
 
 Parser parser;
 CompilerStatus compile(const char *src){
+    addConstant(0);
     //set the ball rolling
     advance();
     expression();
