ff5b1c513946dca497913d81283e0b871540b38c
peterkwesiansah <ansahpeter123@yahoo.com>
2025-10-29 16:01:16 +0000

    pre-add constant literals

--- cbs/compiler.c
+++ cbs/compiler.c
@@ -1,11 +1,14 @@
 #include "compiler.h"
 #include "parser.h"
 #include "readonly.h"
+#include <stdbool.h>
 
 
 CompilerStatus compile(const char *src){
     extern Parser parser;
     addConstant(C_DOUBLE_TO_BS_NUMBER(0));
+    addConstant(C_BOOL_TO_BS_BOOLEAN(true));
+    addConstant(C_BOOL_TO_BS_BOOLEAN(false));
     //Include , true , false and nil
     //set the ball rolling
     advance();
