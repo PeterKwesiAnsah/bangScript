2801ecb6a938b73978c6a350ba3726ac557ad260
peterkwesiansah <ansahpeter123@yahoo.com>
2025-11-28 22:44:07 +0000

    var declarations (wip)

--- cbs/compiler.c
+++ cbs/compiler.c
@@ -2,18 +2,57 @@
 #include "parser.h"
 #include "readonly.h"
 #include <stdbool.h>
+#include "scanner.h"
+#include "table.h"
 
 
 CompilerStatus compile(const char *src){
     extern Parser parser;
+    extern Table strings;
+    Tinit(&strings);
     addConstant(C_DOUBLE_TO_BS_NUMBER(0));
     addConstant(C_BOOL_TO_BS_BOOLEAN(true));
     addConstant(C_BOOL_TO_BS_BOOLEAN(false));
     addConstant((Value){.type=TYPE_NIL,.value={}});
-    //Include , true , false and nil
+
     //set the ball rolling
     advance();
-    expression();
-    //expect parser.current to be "semi colon" token
+    switch (parser.current.tt) {
+        // variable declaration
+        case TOKEN_VAR:
+        {
+            advance();
+            Token cur=parser.current;
+            if(cur.tt!=TOKEN_IDENTIFIER){
+                //compile error
+            }
+            advance();
+            //var foo=0;
+            if(parser.current.tt==TOKEN_EQUAL){
+                advance();
+                expression();
+            }else {
+                 //var foo;
+                if(parser.current.tt!=TOKEN_SEMICOLON){
+                    fprintf(stderr,"Expected a semi-colon but got %d\n",parser.current.tt);
+                    parser.hadError=true;
+                    return true;
+                }
+                advance();
+            }
+
+
+
+        }
+        break;
+
+
+        default:
+            expression();
+        break;
+
+    }
+
+
     return !parser.hadError;
 }
