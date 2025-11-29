01fd710fe96d1718581a165ea0df1d342b5170d2
peterkwesiansah <ansahpeter123@yahoo.com>
2025-11-29 14:43:45 +0000

    introduce new opcodes for handling global variable

--- cbs/chunk.h
+++ cbs/chunk.h
@@ -1,5 +1,8 @@
 #ifndef CHUNK_H
 #define CHUNK_H
+#include "line.h"
+#include "darray.h"
+
 typedef enum {
     OP_CONSTANT_ZER0,
     OP_CONSTANT,
@@ -15,8 +18,19 @@ typedef enum {
     OP_LESS_NOT,
     OP_LESS,
     OP_GREATOR_NOT,
+    OP_GLOBALVAR_DEF,
+    OP_GLOBALVAR_GET,
+    OP_GLOBALVAR_ASSIGN,
     //for debugging purposes
     OP_PRINT,
     OP_RETURN
 } BS_OP_CODES;
+
+DECLARE_ARRAY_TYPE(u_int8_t, Chunk);
+
+#define WRITE_BYTECODE(chunk,byte,line) do{ \
+append(chunk,u_int8_t,byte);\
+addLine(line);\
+}while(0)
+
 #endif
